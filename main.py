d7625873df0d344f878f6958b746032a4eb66c49
KhazAkar <damianzrb@zohomail.eu>
2024-08-19 22:32:23 +0200

    feat: add window, add gitignore, add constants

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
+from constants import *
+
 
 def main():
-    print("Starting asteroids!")
+    numpass, numfail = pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        # Initialize with black color
+        screen.fill(pygame.color.Color(0, 0, 0))
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     main()
