a999ce95a013f697a52bd15324cda92615fffca7
KhazAkar <damianzrb@zohomail.eu>
2024-08-23 23:07:21 +0200

    feat: add circleshape, fix perf issues, add PLAYER_RADIUS

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     numpass, numfail = pygame.init()
+    clk = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -12,7 +14,8 @@ def main():
         # Initialize with black color
         screen.fill(pygame.color.Color(0, 0, 0))
         pygame.display.flip()
-
+        time_passed = clk.tick(60)
+        dt = time_passed / 1000
 
 if __name__ == '__main__':
     main()
