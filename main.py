fbeec82836d559172d30713aa190d4f3585879e7
KhazAkar <damianzrb@zohomail.eu>
2024-08-24 21:05:58 +0200

    chore: change from direct call method to groups

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     numpass, numfail = pygame.init()
     clk = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
@@ -15,8 +18,10 @@ def main():
                 return
         # Initialize with black color
         screen.fill(pygame.color.Color(0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        for updatable_element in updatable:
+            updatable_element.update(dt)
+        for drawable_element in drawable:
+            drawable_element.draw(screen)
         pygame.display.flip()
         time_passed = clk.tick(60)
         dt = time_passed / 1000
