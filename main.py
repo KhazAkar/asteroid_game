bfae682e5a1de5345399e2d5e2e60cde6d633438
KhazAkar <damianzrb@zohomail.eu>
2024-08-24 15:12:06 +0200

    feat: add player, make it rotate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -7,15 +8,19 @@ def main():
     clk = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         # Initialize with black color
         screen.fill(pygame.color.Color(0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         time_passed = clk.tick(60)
         dt = time_passed / 1000
+
 
 if __name__ == '__main__':
     main()
